7265ee7b87738f9a204d1aca5b6b9d2d7ad12d2c
satohi0000 <87967374+satohi0000@users.noreply.github.com>
2022-10-13 12:16:39 +0900

    問い合わせボタンまで

--- js/style.js
+++ js/style.js
@@ -11,11 +11,23 @@ new Splide(".splide", {
   height: "400px",
 }).mount();
 
-$('a[href^="#"]').click(function () {
-  var elmHash = $(this).attr("href"); //ページ内リンクのHTMLタグhrefから、リンクされているエリアidの値を取得
-  var pos = $(elmHash).offset().top + 150 * px; //idの上部の距離を取得
-  $("body,html").animate({ scrollTop: pos }, 500); //取得した位置にスクロール。500の数値が大きくなるほどゆっくりスクロール
-  return false;
+$(function () {
+  // #で始まるa要素をクリックした場合に処理（"#"←ダブルクォーテンションで囲むのを忘れずに。忘れるとjQueryのバージョンによっては動かない。。）
+  $('a[href^="#"]').click(function () {
+    // 移動先を0px調整する。0を30にすると30px下にずらすことができる。
+    var adjust = 0;
+    // スクロールの速度（ミリ秒）
+    var speed = 1000;
+    // アンカーの値取得 リンク先（href）を取得して、hrefという変数に代入
+    var href = $(this).attr("href");
+    // 移動先を取得 リンク先(href）のidがある要素を探して、targetに代入
+    var target = $(href == "#" || href == "" ? "html" : href);
+    // 移動先を調整 idの要素の位置をoffset()で取得して、positionに代入
+    var position = target.offset().top + adjust;
+    // スムーススクロール linear（等速） or swing（変速）
+    $("body,html").animate({ scrollTop: position }, speed, "swing");
+    return false;
+  });
 });
 
 $(".p-article__table-dt1").on("click", function () {
@@ -33,12 +45,12 @@ $(".p-article__table-dt3").on("click", function () {
 
 $(document).ready(function () {
   const $submitBtn = $("#js-submit");
-  $("#contact input,#contact mail,#contact textarea").on("change", function () {
+  $("#form input,#form textarea").on("change", function () {
     if (
-      $('#contact input[type="text"]').val() !== "" &&
-      $('#contact input[type="mail"]').val() !== "" &&
-      $('#contact textarea[type="message"]').val() !== "" &&
-      $("#form #privacycheck").prop("checked") === true
+      $('#form input[type="text"]').val() !== "" &&
+      $('#form input[type="email"]').val() !== "" &&
+      $('#form input[type="checkbox"]').val() !== "" &&
+      $("#form #privacyCheck").prop("checked") === true
     ) {
       $submitBtn.prop("disabled", false);
     } else {
