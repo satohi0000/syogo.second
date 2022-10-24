f51555b77bb94100e62fd5844b37d8d32c98118c
satohi0000 <87967374+satohi0000@users.noreply.github.com>
2022-10-24 17:02:17 +0900

    finish

--- js/style.js
+++ js/style.js
@@ -3,37 +3,88 @@ new Splide(".splide", {
   interval: 5000,
   speed: 2000,
   pauseOnFocus: false,
-  fixedWidth: "33%",
-  gap: "6%",
+  fixedWidth: "35%",
+  gap: "4%",
   padding: "11%",
   type: "loop",
   width: "100%",
   height: "400px",
+  drag: "free",
+  snap: true,
+
   breakpoints: {
-    868: {
-      fixedWidth: "62%",
-      padding: "19%",
+    881: {
+      height: "auto",
     },
     599: {
-      fixedWidth: "95%",
-      padding: "20%",
+      fixedWidth: "80%",
+      padding: "26%",
+      focus: "center",
+      gap: "4%",
     },
   },
 }).mount();
 
 $("#c-BTN1").on("click", function () {
   $(this).toggleClass("active");
-  $(".p-nav-list").toggleClass("open");
+  $(".p-nav-list2").toggleClass("open");
   $(".p-mask").toggleClass("open1");
   $("body").toggleClass("no_scroll");
   return false;
+});
+
+const CHANGE_WIDTH = 881; // 変更を検知する横幅
+const ADD_CLASS = "sp-change"; // 追加するクラス
+
+$(window).on("load resize", function () {
+  var i_width = $(window).outerWidth(true);
+  if (i_width > CHANGE_WIDTH) {
+    if ($(".p-nav-item").hasClass(ADD_CLASS)) {
+      $(".p-nav-item").removeClass(ADD_CLASS);
+    }
+  } else {
+    if (!$(".p-nav-item").hasClass(ADD_CLASS)) {
+      $(".p-nav-item").addClass(ADD_CLASS);
+    }
+  }
+});
+
+$(".p-nav-item a").on("click", function () {
+  $("#c-BTN1").toggleClass("active");
+  $(".p-nav-list2").toggleClass("open");
+  $(".p-mask").toggleClass("open1");
+  $("body").toggleClass("no_scroll");
+  return false;
+});
+
+$(document).ready(function () {
+  $("#form").submit(function (event) {
+    var formData = $("#form").serialize();
+    $.ajax({
+      url: "https://docs.google.com/forms/u/0/d/e/1FAIpQLSc3fZ22K_5d25Tgthov3IFpBxaz2LyhrgQP17ph7E_zcafGCQ/formResponse",
+      data: formData,
+      type: "POST",
+      dataType: "xml",
+      statusCode: {
+        0: function () {
+          $(".c-form__end-message").slideDown();
+          $(".c-button").fadeOut();
+          //window.location.href = "thanks.html";
+        },
+        200: function () {
+          $(".c-form__false-message").slideDown();
+        },
+      },
+    });
+    event.preventDefault();
+  });
 });
 
 $(function () {
   // #で始まるa要素をクリックした場合に処理（"#"←ダブルクォーテンションで囲むのを忘れずに。忘れるとjQueryのバージョンによっては動かない。。）
   $('a[href^="#"]').click(function () {
     // 移動先を0px調整する。0を30にすると30px下にずらすことができる。
-    var adjust = 0;
+    var adjust = -100;
     // スクロールの速度（ミリ秒）
     var speed = 1000;
     // アンカーの値取得 リンク先（href）を取得して、hrefという変数に代入
@@ -75,4 +126,29 @@ $(document).ready(function () {
       $submitBtn.prop("disabled", true);
     }
   });
+});
+
+$("input[name='entry.752881277'").blur(function () {
+  if ($(this).val() == "") {
+    $(".c-form__error-message1").slideDown();
+  } else {
+    $(".c-form__error-message1").slideUp();
+  }
+});
+
+$("input[name='entry.1530546066'").blur(function () {
+  if ($(this).val() == "") {
+    $(".c-form__error-message2").slideDown();
+  } else {
+    $(".c-form__error-message2").slideUp();
+  }
+});
+
+AOS.init({
+  offset: 300,
+  delay: 100,
+  duration: 600,
+  easing: "ease-out",
+  anchorPlacement: "bottom-top",
+  once: false,
 });
